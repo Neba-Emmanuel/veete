d366e60e2babc6d7575e50bddc410700d1fa6d63
Neba Emmanuel <nebaemmanuel736@gmail.com>
2022-11-10 07:05:54 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:veete/screens/living_room/living_room.dart';
 import 'package:veete/widgets/constant.dart';
+import 'package:veete/widgets/colors.dart';
 import 'navigation/navigate.dart';
 
 
@@ -21,6 +23,159 @@ class MyApp extends StatelessWidget {
       routes: Navigate.routes,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.only(top: 18, left: 24, right: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'HI Emmanuel',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: COLORS.kOriginal,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  RotatedBox(
+                    quarterTurns: 135,
+                    child: Icon(
+                      Icons.bar_chart_rounded,
+                      color: COLORS.kOriginal,
+                      size: 28,
+                    ),
+                  )
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    const SizedBox(height: 32),
+                    Center(
+                      child: Image.asset(
+                        'assets/images/land_page.png',
+                        scale: 1.2,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Center(
+                      child: Text(
+                        'Welcome to Veete',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: COLORS.kOriginal,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 48),
+                    const Text(
+                      'SERVICES',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: COLORS.kOriginal,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _cardMenu(
+                          icon: 'assets/images/bed_room.png',
+                          title: ' BED ROOM',
+                        ),
+                        _cardMenu(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const TemperaturePage(),
+                              ),
+                            );
+                          },
+                          icon: 'assets/images/living_room.png',
+                          title: 'LIVING ROOM',
+                          color: COLORS.kOriginal,
+                          fontColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 28),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _cardMenu(
+                          icon: 'assets/images/kitchen.png',
+                          title: 'KITCHEN',
+                        ),
+                        _cardMenu(
+                          icon: 'assets/images/bathroom.png',
+                          title: 'BATH ROOM',
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 28),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardMenu({
+    required String title,
+    required String icon,
+    VoidCallback? onTap,
+    Color color = COLORS.kGreyColor,
+    Color fontColor = COLORS.kBlackColor,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 36,
+        ),
+        width: 150,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Image.asset(icon),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, color: fontColor),
+            )
+          ],
+        ),
       ),
     );
   }
